87c601fe68e01061c837597c15d4be51ae7df77b
vitaliy-ce <ce.vitaliy@gmail.com>
2020-07-23 11:10:24 +0500

    Code refactoring

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -1,40 +1,52 @@
 <?php
 
 /**
- * Генерация хлебных крошек с микроразметкой. Пример передаваемого массива: $links[] = array('name' => 'Услуги', 'link' => '/services'); 
- * 
- * @param  array  $links  Пример передаваемого массива: $links[] = array('name' => 'Услуги', 'link' => '/services'); 
- * @return string         html код хлебных крошек
+ * Генерация хлебных крошек с микроразметкой
  */
-function breadcrumbs($links = array()) {
-	$i = 1;
-	$html = '<ul class="breadcrumb simple" itemscope="" itemtype="http://schema.org/BreadcrumbList">';
-		$html .= '
-			<li class="home_lnk" itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-				<a href="/" itemprop="item"><span itemprop="name">Главная<span></a>
-				<meta itemprop="position" content="'.$i.'">
-			</li>';
-			
-			if (!empty($links)) {
-				foreach ($links as $key => $item) {
-					$i++;
-					if (isset($item['link']) && $item['link'] != '' && ($key +1 ) < count($links)) {
-						$html .= '
-							<li itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-								<a href="'.$item['link'].'" itemprop="item"><span itemprop="name">'.$item['name'].'<span></a>
-								<meta itemprop="position" content="'.$i.'">
-							</li>';							
-					} else {
-						$html .= '
-							<li itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
-								<span itemprop="name">'.$item['name'].'<span>
-								<meta itemprop="item" content="https://'.$_SERVER['SERVER_NAME'].parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH).'">
-								<meta itemprop="position" content="'.$i.'">
-							</li>';								
-					}
+class Breadcrumbs {
+	private $links;
+
+	public function __construct($add_home_link = true) {
+		$this->$links = [];
+		if ($add_home_link) {
+			$this->addLink('Главная', '/');			
+		}
+	}
+
+	public function addLink($name, $link = '') {
+		$this->$links[] = [
+			'name' => trim($name),
+			'link' => trim($link),
+		];
+	}
+
+	public function getHtml() {
+		$html = '';
+
+		if (!empty($this->$links)) {
+			$html = '<ul class="breadcrumb" itemscope="" itemtype="http://schema.org/BreadcrumbList">';
+		
+			foreach ($this->$links as $key => $item) {
+				if (isset($item['link']) && $item['link'] != '' && ($key+1) < count($this->$links)) {
+					$html .= '
+						<li itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+							<a href="'.$item['link'].'" itemprop="item"><span itemprop="name">'.$item['name'].'<span></a>
+							<meta itemprop="position" content="'.($key+1).'">
+						</li>
+					';							
+				} else {
+					$html .= '
+						<li itemscope="" itemprop="itemListElement" itemtype="http://schema.org/ListItem">
+							<span itemprop="name">'.$item['name'].'<span>
+							<meta itemprop="item" content="'.$item['link'].'">
+							<meta itemprop="position" content="'.($key+1).'">
+						</li>
+					';								
 				}
 			}
-	$html .= '</ul>';
+			$html .= '</ul>';
+		}
 	
-	return $html;
+		return $html;
+	}
 }
